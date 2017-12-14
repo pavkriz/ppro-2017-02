c9ba4da0bca47ea45ec76e2ec4f082fe9856a624
pavkriz <pavkriz@hkfree.org>
2017-12-14 13:12:25 +0100

    minor text change (Hi->Ahoj)

--- src/main/java/cz/uhk/ppro/rest2/services/GreetingService.java
+++ src/main/java/cz/uhk/ppro/rest2/services/GreetingService.java
@@ -12,7 +12,7 @@ public class GreetingService {
     private final AtomicLong counter = new AtomicLong();
 
     public GreetingService() {
-        this.template = "Hi, %s!";
+        this.template = "Ahoj, %s!";
     }
 
     public GreetingService(String template) {
